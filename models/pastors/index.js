5bf6b33eb24f830d7b51c2c74d96e1b67e170256
unknown <jean-pierre14>
2020-10-16 14:37:19 +0200

    Pastor endpoint

--- models/pastors/index.js
+++ models/pastors/index.js
@@ -1,6 +1,13 @@
 const db = require('../')
 
 class Pastors{
+    static save(id, name, callback){
+        let SQL = "INSERT INTO pastor_tb SET user_id=?, pastor_name=?"
+        db.query(SQL, [id, name], (e, result)=>{
+            if(e) throw e
+            callback(result)
+        })
+    }
     static find(callback){
         let SQL = "SELECT * FROM pastor_tb ORDER BY id DESC"
         db.query(SQL, (err, results)=>{
@@ -9,21 +16,23 @@ class Pastors{
         })
     }
     static findOne(id, callback){
-        let SQL = "SELECT * FROM pastor_tb WHERE id = ?"
+        let SQL = "SELECT * FROM pastor_tb WHERE user_id = ?"
         db.query(SQL, [id],(err, result)=>{
             if(err) throw err
             callback(result)
         })
     }
-    static updateOne(name,id, callback) {
-        let SQL = "UPDATE pastor_tb SET pastor_name = ? WHERE id = ?"
+
+    static updateOne(id, name, callback) {
+        let SQL = "UPDATE pastor_tb SET pastor_name = ? WHERE user_id = ?"
+        console.log(id, name)
         db.query(SQL, [name, id], (err, result)=>{
             if(err) throw err
             callback(result)
         })
     }
     static deleteOne(id, callback){
-        let SQL = "DELETE FROM pastor_tb WHERE id = ?"
+        let SQL = "DELETE FROM pastor_tb WHERE user_id = ?"
         db.query(SQL, [id], (err, result)=>{
             if(err) throw err
             callback(result)
--- routes/Pastors.js
+++ routes/Pastors.js
@@ -2,20 +2,50 @@ const exp = require('express')
 const router = exp.Router()
 const Pastors = require('../models/pastors/')
 
-router.get('/', (req, res)=>{
-    Pastors.find((results)=>{
+router.get('/', async (req, res)=>{
+    await Pastors.find((results)=>{
         res.json(results)
     })
 })
 
-router.get('/:id', (req, res)=>{
+router.post('/', async (req, res)=>{
+    const {id,name} = req.body
+    if(!id || !name){
+        res.json({message: "Empty fields"})
+    }else{
+        await Pastors.save(id, name, (result)=>{
+            res.json(result)
+        })
+    }
+})
+
+router.get('/:id', async (req, res)=>{
     let id = req.params.id
-    Pastors.findOne(id,(result)=>{
+    await Pastors.findOne(id,(result)=>{
         res.json(result)
     })
 })
 
-// UpdateOne
+router.put('/:id', (req, res)=>{
+    let id = req.params.id
+    let name = req.body.name
+    Pastors.updateOne(id, name, (result)=>{
+        console.log(id, name)
+        res.json(result)
+    })
+})
+
+router.delete('/:id', async (req, res)=>{
+    let id = req.params.id
+    await Pastors.deleteOne(id, (result)=>{
+        res.json(result)
+    })
+})
+router.delete('/delete', async (req, res)=>{
+    await Pastors.deleteAll((result)=>{
+        res.json(result)
+    })
+})
 // DeleteOne
 // DeleteAll
 
