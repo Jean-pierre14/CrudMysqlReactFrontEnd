1b1b94fc4d78079992336670b42a104088943006
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-12-06 12:29:21 +0200

    get and post

---
+++ public/js/axios.js
@@ -0,0 +1,41 @@
+function getData() {
+    axios({
+        method: 'get',
+        url: 'http://localhost:5000/programmes'
+    }).then(res => showData(res)).catch(err => { console.log(err) })
+}
+
+function showData(res) {
+    document.getElementById('results').innerHTML = `
+        <div class="card my-3">
+            <h2>Status ${res.status}</h2>
+        </div>
+        <div class="card card-body my-3">
+            <pre><small>Status ${JSON.stringify(res.data, null, 2)}</small></pre>
+        </div>
+    `
+}
+
+document.getElementById('get').addEventListener('click', getData)
+document.getElementById('post').addEventListener('click', postData)
+let error = document.getElementById('error')
+// POST
+function postData() {
+    let userID = document.getElementById('userID').value
+    let title = document.getElementById('title').value
+    let content = document.getElementById('content').value
+
+    if (!userID || !title || !content) {
+        alert('all fields are required')
+    } else {
+        axios({
+            method: 'post',
+            url: 'http://localhost:5000/programmes',
+            data: {
+                userID,
+                title,
+                content
+            }
+        }).then(res=> showData(res)).catch(err=>{console.log(err)})
+    }
+}
