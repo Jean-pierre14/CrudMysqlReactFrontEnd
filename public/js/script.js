ebbe2fca853d360efb1075ff520c160bd13079d1
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-12-13 10:06:39 +0200

    Second UI

--- public/js/script.js
+++ public/js/script.js
@@ -7,38 +7,13 @@ form.addEventListener('click', function (e) {
     e.preventDefault()
     checkInputs()
 })
+
 function checkInputs() {
     const nameValue = name.value.trim()
-    const emValue = email.value.trim()
-    const ageValue = age.value.trim()
-    if (nameValue === '') {
-        setErrorFor(name, 'Nom est vide')
+
+    if (nameValue === undefined || nameValue === null) {
+        setError(name, "Le nom est vide")   
     } else {
-        setSuccessFor(name)
-    }
-    if (emValue === '' || emValue === undefined) {
-        setErrorFor(email, "Votre email")
-    } else {
-        setSuccessFor(email)
-    }
-    if (ageValue === '') {
-        setErrorFor(input, "Age est invalid")
-    } else {
-        setSuccessFor(input)
+        alert("Pass")
     }
 }
-
-function setErrorFor(input, msg) {
-    const form = input.parentElement
-    const small = form.querySelector('small')
-    small.innerText = msg
-    // add the class error
-    form.className = 'form error'
-}
-
-function setSuccessFor(input) {
-    const form = form.parentElement
-    form.className = 'form success'
-}
-
-// check the email isEmail
