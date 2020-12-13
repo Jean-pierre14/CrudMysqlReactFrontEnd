b061f97979ef9e89d70522d604160d19db9f4845
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-12-13 07:14:00 +0200

    Script validation

---
+++ public/js/script.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('submit')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const pass = document.getElementById('pass')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    
+    checkInputs()
+})
+
+function checkInputs() {
+    const userValue = username.value.trim()
+    const emValue = email.value.trim()
+    const psValue = pass.value.trim()
+
+    // Check the username
+    if (userValue === '') {
+        // Display error
+        // Add our error class
+        setErrorFor(username, 'Username is blank')
+    } else {
+        setSuccessFor(username)
+    }
+    if (emValue === '' || emValue === undefined) {
+        setErrorFor(email, "Email is blank")
+    } else {
+        setSuccessFor(email)
+    }
+
+    if (psValue === '') {
+        setErrorFor(input, "Password is required")
+    } else {
+        setSuccessFor(input)
+    }
+}
+
+function setErrorFor(input, msg) {
+    const group = input.parentElement
+    const small = group.querySelector('small')
+    small.innerText = msg
+    // add the class error
+    group.className = 'group error'
+}
+
+function setSuccessFor(input) {
+    const group = input.parentElement
+    group.className = 'group success'
+}
+
+// check the email isEmail
